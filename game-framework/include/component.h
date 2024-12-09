870f686cb904059824c992f45ece0a94dce121d0
Syro <network.9961@gmail.com>
2024-12-09 15:45:15 -0600

    Add sfml and vpckg manifest for windows build

--- game-framework/include/component.h
+++ game-framework/include/component.h
@@ -1,3 +1,15 @@
 #pragma once
 
-// to do...
+#include <SFML/Graphics.hpp>
+
+void createWindow() {
+    sf::Window window(sf::VideoMode(560, 720), "Pac-man clone");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+    }
+}
